161afb49f62135f28e5769a61c4ff4acc6c50b97
faiz <javafaiz18@gmail.com>
2023-05-20 20:13:59 +0530

    This is my second App

---
+++ src/main/java/in/mubarak/SpringbokMailAppApplication.java
@@ -0,0 +1,21 @@
+package in.mubarak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbokMailAppApplication {
+
+	@GetMapping
+	public String getMess(){
+		return "Welcome to automate pipeline in jenkins";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbokMailAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/mubarak/SpringbootMailAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.mubarak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMailAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
